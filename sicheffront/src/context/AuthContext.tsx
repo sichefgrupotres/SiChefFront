ea1a11cb391606672e7f7264d894ba643ad7e2bb
MaxiLevy <maximolevy11@gmail.com>
2025-12-18 22:59:11 -0300

    authcontext

--- sicheffront/src/context/AuthContext.tsx
+++ sicheffront/src/context/AuthContext.tsx
@@ -1,107 +1,67 @@
-// "use client";
+'use client';
+import { userSessionInterface } from "@/interfaces/IUser";
+import { createContext, useState, useEffect, useContext, ReactNode, } from "react";
 
-// import { createContext, useContext, useEffect, useState, ReactNode } from "react";
 
-// export type Role = "CREATOR" | "USER";
-// export type Genero = "masculino" | "femenino" | "no_binario"| "no_responder"
+interface AuthContextProps {
+    dataUser: userSessionInterface | null;
+    setDataUser: (dataUser: userSessionInterface | null) => void;
+    logout: () => void;
+    loading: boolean;
+    isLoadingUser: boolean;
+}
 
-// export interface RegisterFormData {
-//   nombre: string;
-//   apellido: string;
-//   email: string;
-//   password: string;
-//   confirmPassword: string;
-//   cumpleaños: string;
-//   genero: Genero;
-//   nacionalidad: string;
-//   ciudad: string;
-//   paisDeResidencia: string;
-//   avatarUrl: string;
-//   role: Role;
-// }
+const AuthContext = createContext<AuthContextProps>({
+    dataUser: null,
+    setDataUser: () => { },
+    logout: () => { },
+    loading: true,
+    isLoadingUser: true
 
-// interface User {
-//   id: string;
-//   nombre: string;
-//   apellido: string;
-//   email: string;
-//   role: Role;
-// }
+});
 
-// interface AuthContextType {
-//   user: User | null;
-//   token: string | null;
-//   login: (email: string, password: string) => Promise<void>;
-//   register: (data: RegisterFormData) => Promise<void>;
-//   logout: () => void;
-// }
 
-// const AuthContext = createContext<AuthContextType | null>(null);
+interface AuthProviderProps {
+    children: ReactNode;
+}
 
-// export function AuthProvider({ children }: { children: ReactNode }) {
-//   const [user, setUser] = useState<User | null>(null);
-//   const [token, setToken] = useState<string | null>(null);
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+    const [dataUser, setDataUser] = useState<userSessionInterface | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [isLoadingUser, setIsLoadingUser] = useState(true)
 
-//   useEffect(() => {
-//     const storedToken = localStorage.getItem("token");
-//     if (!storedToken) return;
+    useEffect(() => {
+        //se encarga de extraer la informacion del localStorage cuando se recarga la pagina y almacenar en el estado
+        if (typeof window !== 'undefined' && window.localStorage) {
+            const userInfo = localStorage.getItem('userSession');
+            if (userInfo) {
+                setDataUser(JSON.parse(userInfo));
+            }
+        }
+        setLoading(false)
+        setIsLoadingUser(false)
+    }, []);
 
-//     fetch("http://localhost:3000/auth/me", {
-//       headers: { Authorization: `Bearer ${storedToken}` },
-//     })
-//       .then(res => res.ok ? res.json() : Promise.reject())
-//       .then(data => {
-//         setUser(data.user);
-//         setToken(storedToken);
-//       })
-//       .catch(() => localStorage.removeItem("token"));
-//   }, []);
 
-//   const register = async (data: RegisterFormData) => {
-//     const res = await fetch("http://localhost:3000/auth/register", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify(data),
-//     });
+    useEffect(() => {
+        // se ejecutara cuando mi dataUser cambie, y lo guardara en el localStorage
+        if (dataUser) {
+            localStorage.setItem('userSession', JSON.stringify(dataUser));
+        }
+    }, [dataUser]);
 
-//     if (!res.ok) throw new Error("Error en registro");
 
-//     const result = await res.json();
-//     setUser(result.user);
-//     setToken(result.token);
-//     localStorage.setItem("token", result.token);
-//   };
+    const logout = () => {
+        setDataUser(null);
+        if (typeof window !== 'undefined' && window.localStorage) {
+            localStorage.removeItem('userSession');
+        }
+    }
+    return (
+        <AuthContext.Provider value={{ dataUser, setDataUser, logout, loading, isLoadingUser }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
 
-//   const login = async (email: string, password: string) => {
-//     const res = await fetch("http://localhost:3000/auth/login", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({ email, password }),
-//     });
-
-//     if (!res.ok) throw new Error("Credenciales inválidas");
-
-//     const result = await res.json();
-//     setUser(result.user);
-//     setToken(result.token);
-//     localStorage.setItem("token", result.token);
-//   };
-
-//   const logout = () => {
-//     setUser(null);
-//     setToken(null);
-//     localStorage.removeItem("token");
-//   };
-
-//   return (
-//     <AuthContext.Provider value={{ user, token, login, register, logout }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// }
-
-// export const useAuth = () => {
-//   const ctx = useContext(AuthContext);
-//   if (!ctx) throw new Error("useAuth debe usarse dentro de AuthProvider");
-//   return ctx;
-// };
+export const useAuth = () => useContext(AuthContext);
--- sicheffront/src/interfaces/IRepice.ts
+++ sicheffront/src/interfaces/IRepice.ts
@@ -4,4 +4,5 @@ export interface RecipeInterface {
   // image: string
   difficulty: "facil" | "medio" | "dificil";
   isPremium: boolean;
+
 }
