a1f7c533fda8e9b50205a882b7d0b9d487c111ca
CarolinaAT <carolinaarizat93@gmail.com>
2025-12-22 02:46:49 -0300

    Creacion de ruta para autenticacion con Nextauth en la aplicacion

--- sicheffront/src/app/api/auth/[...nextauth]/route.ts
+++ sicheffront/src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,22 @@
-// app/api/auth/[...nextauth]/route.ts
-import { handlers } from "@/auth"  // apunta a auth.ts
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProviders from "next-auth/providers/credentials";
 
-export const { GET, POST } = handlers
+export const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_0AUTH_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_0AUTH_CLIENT_SECRET!,
+    }),
+    CredentialsProviders({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+    })
+  ],
+  
+})
+
+export {handler as GET, handler as POST};
