3bd09a34384e3e590eb6ab6f3762434c610c0b2d
yeendj <yeendj@gmail.com>
2026-01-15 00:44:28 -0500

    Revert "se agrego propiedad a la interfaz para el recipeId relacionado"
    This reverts commit e4c3968b004f321f425de1cdd5b40d5b4f3c58ae.

--- sicheffront/src/interfaces/ITutorial.ts
+++ sicheffront/src/interfaces/ITutorial.ts
@@ -2,11 +2,6 @@ export interface TutorialInterface {
   id: string;
   title: string;
   videoUrl: string;
-   recipeId: string;
-  recipe?: {
-    id: string;
-    title: string;
-  };
   ingredients: {
     title: string;
     description?: string;
